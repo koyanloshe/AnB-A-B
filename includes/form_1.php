7e70a8a01b5f3d2c4bec83b3cb06e8451c3e65dc
koyanloshe <koyanloshe@gmail.com>
2016-07-26 22:03:03 +0530

    git initialise

---
+++ includes/form_1.php
@@ -0,0 +1,22 @@
+<?php	
+	if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['message']))
+	{
+		return false;
+	}
+	
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$message = $_POST['message'];
+	
+	$to = 'receiver@yoursite.com'; // Email submissions are sent to this email
+
+	// Create email	
+	$email_subject = "Message from 2016.";
+	$email_body = "You have received a new message. \n\n".
+				  "Name: $name \nEmail: $email \nMessage: $message \n";
+	$headers = "From: contact@yoursite.com\n";
+	$headers .= "Reply-To: $email";	
+	
+	mail($to,$email_subject,$email_body,$headers); // Post message
+	return true;			
+?>
